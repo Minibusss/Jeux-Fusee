652dcaeb2dda71f507427ddb5421f03adb0eee92
Minibusss <nicolas.cirou@reseau.eseo.fr>
2024-03-05 10:45:14 +0100

    first commit

---
+++ Main_fusee.py
@@ -0,0 +1,14 @@
+import pygame
+import sys
+import module_fusee as ma
+
+
+
+ma.Jeux_niveau_debutant()
+ma.Jeux_niveau_intermediaire()
+ma.Jeux_niveau_grands_sage_légendaire()
+
+
+# Quitte Pygame
+pygame.quit()
+sys.exit()
